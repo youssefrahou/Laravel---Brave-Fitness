18a088e42857f0aa75af9e3fba56a70802050221
Youssef <youssefra3@gmail.com>
2020-05-09 03:56:52 +0200

    antes de cagarla xd

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -50,12 +50,16 @@ class RegisterController extends Controller
      */
     protected function validator(array $data)
     {
+        
         return Validator::make($data, [
             'name' => ['required', 'string', 'max:255'],
             'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
             'password' => ['required', 'string', 'min:8', 'confirmed'],
         ]);
     }
+
+
+
 
     /**
      * Create a new user instance after a valid registration.
