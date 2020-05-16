90ae91b75522fb898a4f70b9d811b2d20f71181e
Youssef <youssefra3@gmail.com>
2020-05-16 02:03:10 +0200

    añadido chat en areaPersonal

--- resources/views/areaPersonal.blade.php
+++ resources/views/areaPersonal.blade.php
@@ -41,6 +41,43 @@ $(".file-upload").on('change', function(){
         cursor: pointer;
     }
 </style>
+<!-- CHAT AQUI ABAJO -->
+<style>
+    ::-webkit-scrollbar {
+        width: 5px;
+    }
+
+    ::-webkit-scrollbar-track {
+        width: 5px;
+        background: #f5f5f5;
+    }
+
+    ::-webkit-scrollbar-thumb {
+        width: 1em;
+        background-color: #ddd;
+        outline: 1px solid slategrey;
+        border-radius: 1rem;
+    }
+
+    .text-small {
+        font-size: 0.9rem;
+    }
+
+    .messages-box,
+    .chat-box {
+        height: 510px;
+        overflow-y: scroll;
+    }
+
+    .rounded-lg {
+        border-radius: 0.5rem;
+    }
+
+    input::placeholder {
+        font-size: 0.9rem;
+        color: #999;
+    }
+</style>
 @stop
 
 @section('lateral')
@@ -578,7 +615,187 @@ $(".file-upload").on('change', function(){
 
     <div class="row col-12" id="chat" style="display: none">
 
-        chattyguhijokpl
+        <div class="container-fluid px-4">
+            <!-- For demo purpose-->
+
+            <div class="row rounded-lg overflow-hidden shadow">
+
+
+                <div class="col-12 px-0">
+                    <div class="px-4 py-5 chat-box bg-white">
+                        <!-- Sender Message-->
+                        <div class="media w-100 mb-3"><img
+                                src="https://res.cloudinary.com/mhmd/image/upload/v1564960395/avatar_usae7z.svg"
+                                alt="user" width="50" class="rounded-circle">
+                            <div class="media-body ml-3">
+                                <div class="bg-light rounded py-2 px-3 mb-2">
+                                    <p class="text-small mb-0 text-muted">Test which is a new approach all solutions</p>
+                                </div>
+                                <p class="small text-muted">12:00 PM | Aug 13</p>
+                            </div>
+                        </div>
+
+                        <!-- Reciever Message-->
+                        <div class="media w-100 ml-auto mb-3">
+                            <div class="media-body">
+                                <div class="bg-primary rounded py-2 px-3 mb-2">
+                                    <p class="text-small mb-0 text-white">Test which is a new approach to have all
+                                        solutions</p>
+                                </div>
+                                <p class="small text-muted">12:00 PM | Aug 13</p>
+                            </div>
+                        </div>
+
+                        <!-- Sender Message-->
+                        <div class="media w-100 mb-3"><img
+                                src="https://res.cloudinary.com/mhmd/image/upload/v1564960395/avatar_usae7z.svg"
+                                alt="user" width="50" class="rounded-circle">
+                            <div class="media-body ml-3">
+                                <div class="bg-light rounded py-2 px-3 mb-2">
+                                    <p class="text-small mb-0 text-muted">Test, which is a new approach to have</p>
+                                </div>
+                                <p class="small text-muted">12:00 PM | Aug 13</p>
+                            </div>
+                        </div>
+
+                        <!-- Reciever Message-->
+                        <div class="media w-100 ml-auto mb-3">
+                            <div class="media-body">
+                                <div class="bg-primary rounded py-2 px-3 mb-2">
+                                    <p class="text-small mb-0 text-white">Apollo University, Delhi, India Test</p>
+                                </div>
+                                <p class="small text-muted">12:00 PM | Aug 13</p>
+                            </div>
+                        </div>
+
+                        <!-- Sender Message-->
+                        <div class="media w-100 mb-3"><img
+                                src="https://res.cloudinary.com/mhmd/image/upload/v1564960395/avatar_usae7z.svg"
+                                alt="user" width="50" class="rounded-circle">
+                            <div class="media-body ml-3">
+                                <div class="bg-light rounded py-2 px-3 mb-2">
+                                    <p class="text-small mb-0 text-muted">Test, which is a new approach</p>
+                                </div>
+                                <p class="small text-muted">12:00 PM | Aug 13</p>
+                            </div>
+                        </div>
+
+                        <!-- Reciever Message-->
+                        <div class="media w-100 ml-auto mb-3">
+                            <div class="media-body">
+                                <div class="bg-primary rounded py-2 px-3 mb-2">
+                                    <p class="text-small mb-0 text-white">Apollo University, Delhi, India Test</p>
+                                </div>
+                                <p class="small text-muted">12:00 PM | Aug 13</p>
+                            </div>
+                        </div>
+                        <!-- Sender Message-->
+                        <div class="media w-100 mb-3"><img
+                                src="https://res.cloudinary.com/mhmd/image/upload/v1564960395/avatar_usae7z.svg"
+                                alt="user" width="50" class="rounded-circle">
+                            <div class="media-body ml-3">
+                                <div class="bg-light rounded py-2 px-3 mb-2">
+                                    <p class="text-small mb-0 text-muted">Test, which is a new approach</p>
+                                </div>
+                                <p class="small text-muted">12:00 PM | Aug 13</p>
+                            </div>
+                        </div>
+
+                        <!-- Reciever Message-->
+                        <div class="media w-100 ml-auto mb-3">
+                            <div class="media-body">
+                                <div class="bg-primary rounded py-2 px-3 mb-2">
+                                    <p class="text-small mb-0 text-white">Apollo University, Delhi, India Test</p>
+                                </div>
+                                <p class="small text-muted">12:00 PM | Aug 13</p>
+                            </div>
+                        </div><!-- Sender Message-->
+                        <div class="media w-100 mb-3"><img
+                                src="https://res.cloudinary.com/mhmd/image/upload/v1564960395/avatar_usae7z.svg"
+                                alt="user" width="50" class="rounded-circle">
+                            <div class="media-body ml-3">
+                                <div class="bg-light rounded py-2 px-3 mb-2">
+                                    <p class="text-small mb-0 text-muted">Test, which is a new approach</p>
+                                </div>
+                                <p class="small text-muted">12:00 PM | Aug 13</p>
+                            </div>
+                        </div>
+
+                        <!-- Reciever Message-->
+                        <div class="media w-100 ml-auto mb-3">
+                            <div class="media-body">
+                                <div class="bg-primary rounded py-2 px-3 mb-2">
+                                    <p class="text-small mb-0 text-white">Apollo University, Delhi, India Test</p>
+                                </div>
+                                <p class="small text-muted">12:00 PM | Aug 13</p>
+                            </div>
+                        </div><!-- Sender Message-->
+                        <div class="media w-100 mb-3"><img
+                                src="https://res.cloudinary.com/mhmd/image/upload/v1564960395/avatar_usae7z.svg"
+                                alt="user" width="50" class="rounded-circle">
+                            <div class="media-body ml-3">
+                                <div class="bg-light rounded py-2 px-3 mb-2">
+                                    <p class="text-small mb-0 text-muted">Test, which is a new approach</p>
+                                </div>
+                                <p class="small text-muted">12:00 PM | Aug 13</p>
+                            </div>
+                        </div>
+
+                        <!-- Reciever Message-->
+                        <div class="media w-100 ml-auto mb-3">
+                            <div class="media-body">
+                                <div class="bg-primary rounded py-2 px-3 mb-2">
+                                    <p class="text-small mb-0 text-white">Apollo University, Delhi, India Test</p>
+                                </div>
+                                <p class="small text-muted">12:00 PM | Aug 13</p>
+                            </div>
+                        </div><!-- Sender Message-->
+                        <div class="media w-100 mb-3"><img
+                                src="https://res.cloudinary.com/mhmd/image/upload/v1564960395/avatar_usae7z.svg"
+                                alt="user" width="50" class="rounded-circle">
+                            <div class="media-body ml-3">
+                                <div class="bg-light rounded py-2 px-3 mb-2">
+                                    <p class="text-small mb-0 text-muted">Test, which is a new approach</p>
+                                </div>
+                                <p class="small text-muted">12:00 PM | Aug 13</p>
+                            </div>
+                        </div>
+
+                        <!-- Reciever Message-->
+                        <div class="media w-100 ml-auto mb-3">
+                            <div class="media-body">
+                                <div class="bg-primary rounded py-2 px-3 mb-2">
+                                    <p class="text-small mb-0 text-white">Apollo University, Delhi, India Test</p>
+                                </div>
+                                <p class="small text-muted">12:00 PM | Aug 13</p>
+                            </div>
+                        </div>
+
+
+
+
+                    </div>
+
+                    <!-- Typing area -->
+                    <form action="#" class="bg-light">
+                        <div class="input-group">
+                            <input type="text" placeholder="Escribe un mensaje" aria-describedby="button-addon2"
+                                class="form-control rounded-0 border-0 py-4 bg-light">
+                            <div class="input-group-append">
+                                <button id="button-addon2" type="submit" class="btn btn-link"> <i
+                                        class="fa fa-paper-plane"></i></button>
+                            </div>
+                        </div>
+                    </form>
+
+                </div>
+            </div>
+        </div>
+
+
+
+
+
 
     </div>
     <!-- CHAT -->
